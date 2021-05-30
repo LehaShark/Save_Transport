e9bc79565d87de55ad1f7e21c87351dbc1f7fcec
LehaShark <alexey99979@yandex.ru>
2021-05-30 16:18:10 +0300

    live on py format

--- main_classifier/live.py
+++ main_classifier/live.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import numpy as np
@@ -12,18 +12,64 @@ import matplotlib.pyplot as plt
 import librosa.display
 from tensorflow.keras.models import load_model
 import keyboard
-
-
-# In[2]:
 import cv2
-
-def preprocessDataForNN(img_arr): 
-    f = cv2.resize(img_arr, (150, 150))
-    res = f.reshape(-1, 150, 150, 3)
-    return res
+import io
+import wave
 
 
 # In[3]:
+
+
+def get_img_from_fig(fig, dpi=400):
+    buf = io.BytesIO()
+    fig.savefig(buf, format="png", dpi=dpi)
+    buf.seek(0)
+    img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+    buf.close()
+    img = cv2.imdecode(img_arr, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    return img
+
+
+# In[15]:
+
+
+def preprocessDataForNN(img_arr): 
+    
+    img_arr = img_arr/255.0
+    f = cv2.resize(img_arr, (150, 150))
+    res = f.reshape(-1, 150, 150, 3)
+    
+    return res
+
+
+# In[5]:
+
+
+def createSpectrogramToBuffer(y, sample_rate):
+    
+    plt.interactive(False)
+        
+    fig = plt.figure(figsize=[0.72,0.72])
+    ax = fig.add_subplot(111)
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.set_frame_on(False)
+    spectrogram = librosa.feature.melspectrogram(y=y, sr=sample_rate)
+    librosa.display.specshow(librosa.power_to_db(spectrogram, ref=np.max))
+
+    arr = get_img_from_fig(fig)
+    plt.close()    
+    fig.clf()
+    plt.close(fig)
+    plt.close('all')
+  
+    return arr
+
+
+# In[6]:
+
 
 
 def create_spectrogram(y, sample_rate):
@@ -43,17 +89,11 @@ def create_spectrogram(y, sample_rate):
     plt.close(fig)
     plt.close('all')
 
-    del sample_rate,fig,ax
-    
     return spectrogram
-    
 
 
-# In[7]:
+# In[16]:
 
-
-import pyaudio
-import wave
 
 CHUNK = 1024
 FORMAT = pyaudio.paFloat32
@@ -61,21 +101,6 @@ CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 1
 
-p = pyaudio.PyAudio()
-
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
-
-print("* recording")
-
-CHUNK = 1024
-FORMAT = pyaudio.paFloat32
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 1.5
 #WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
@@ -83,13 +108,15 @@ stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
-                frames_per_buffer=CHUNK)
+                frames_per_buffer=CHUNK,
+                input_device_index = 1) # change this parameter to good work
 
 print("* recording")
 
 frames = []
 
 model = load_model(r'D:\save_1.h5')
+#model = load_model('/home/aigaf/Downloads/Telegram Desktop/save_1.h5')
 
 while(True):  
     frames = []  
@@ -102,24 +129,23 @@ while(True):
     frames = bytes(frames)
 
     numpy_array = np.frombuffer(frames, dtype=np.float32)
-    sp = create_spectrogram(numpy_array, RATE)
-     
+    
+    sp = create_spectrogram(numpy_array, RATE)     
     plt.imshow(sp, interpolation='nearest') 
     plt.show() 
     librosa.display.specshow(librosa.power_to_db(sp, ref=np.max))
     
-    print(sp.shape)
-    
-    img_compl = preprocessDataForNN(sp)
+    img_array = createSpectrogramToBuffer(numpy_array,RATE)    
+    img_compl = preprocessDataForNN(img_array)
     final_res = model.predict(img_compl)
     print(final_res)
     
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-            print('Exit')
-            break  # finishing the loop
-    except:
-        break  # if user pressed a key other than the given key the loop will break
+    #try:  # used try so that if user pressed other than the given key error will not be shown
+        #if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+            #print('Exit')
+            #break  # finishing the loop
+    #except:
+        #break  # if user pressed a key other than the given key the loop will break
     #print(sp)
 
 # numpy_array = np.frombuffer(pull, dtype=np.float32)
@@ -131,7 +157,7 @@ while(True):
 p.terminate()
 
 
-# In[5]:
+# In[7]:
 
 
 from matplotlib import pyplot as plt 
@@ -140,7 +166,7 @@ plt.show()
 librosa.display.specshow(librosa.power_to_db(sp, ref=np.max))
 
 
-# In[9]:
+# In[1]:
 
 
 #1
@@ -167,14 +193,16 @@ cv.waitKey(0)
 cv.destroyAllWindows()
 
 
-# In[14]:
+# In[8]:
 
 
-import imageio
-import numpy
-img = imageio.imread(r'D:\reposetory\Save_Transport\dataset\train\one_sec_cut.jpg\0_1_01.jpg')
-array = numpy.asarray(img)
-print(array.shape)
+# get information about audio devices
+p = pyaudio.PyAudio()
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i))
 
 
 # In[ ]:
