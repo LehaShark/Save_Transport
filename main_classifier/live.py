0776bcf44087209e39d8c70b085d0468c0d05752
TIKki43 <timur430034space@gmail.com>
2021-06-01 21:40:32 +0300

    asd

--- main_classifier/live.py
+++ main_classifier/live.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import pyaudio
 import time
@@ -11,19 +5,16 @@ import librosa
 import matplotlib.pyplot as plt
 import librosa.display
 from tensorflow.keras.models import load_model
-import keyboard
 
 
-# In[2]:
 import cv2
 
-def preprocessDataForNN(img_arr): 
-    f = cv2.resize(img_arr, (150, 150))
+
+def preprocessDataForNN(img_arr):
+    img_arr = img_arr*(1/.255)
+    f = np.resize(img_arr, (150, 150, 3))
     res = f.reshape(-1, 150, 150, 3)
     return res
-
-
-# In[3]:
 
 
 def create_spectrogram(y, sample_rate):
@@ -76,7 +67,7 @@ FORMAT = pyaudio.paFloat32
 CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 1.5
-#WAVE_OUTPUT_FILENAME = "output.wav"
+
 
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT,
@@ -89,13 +80,13 @@ print("* recording")
 
 frames = []
 
-model = load_model(r'D:\save_1.h5')
+model = load_model(r'/home/timur/Documents/Projects/sound_classif/save_1.h5')
 
-while(True):  
-    frames = []  
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.extend(data)
+# while(True):
+#     frames = []
+#     for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+#         data = stream.read(CHUNK)
+#         frames.extend(data)
     
     #print("* done recording")
 
@@ -103,34 +94,20 @@ while(True):
 
     numpy_array = np.frombuffer(frames, dtype=np.float32)
     sp = create_spectrogram(numpy_array, RATE)
-     
-    plt.imshow(sp, interpolation='nearest') 
-    plt.show() 
+
+    plt.imshow(sp, interpolation='nearest')
+    # plt.show()
     librosa.display.specshow(librosa.power_to_db(sp, ref=np.max))
-    
     print(sp.shape)
-    
-    img_compl = preprocessDataForNN(sp)
+    # img_compl = preprocessDataForNN(sp)
     final_res = model.predict(img_compl)
     print(final_res)
-    
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-            print('Exit')
-            break  # finishing the loop
-    except:
-        break  # if user pressed a key other than the given key the loop will break
-    #print(sp)
+    break
 
 # numpy_array = np.frombuffer(pull, dtype=np.float32)
 # sp = create_spectrogram(numpy_array, RATE)
 # print(sp)
-
-#stream.stop_stream()
-##stream.close()
 p.terminate()
-
-
 # In[5]:
 
 
@@ -140,12 +117,6 @@ plt.show()
 librosa.display.specshow(librosa.power_to_db(sp, ref=np.max))
 
 
-# In[9]:
-
-
-#1
-#img - numpy array spectrogram
-
 from sys import argv
 import cv2 as cv
 import numpy as np
@@ -154,14 +125,13 @@ img = sp
 
 ret, threshold = cv.threshold(img, 120, 255, cv.THRESH_BINARY)
 
-cv.imshow('threshold', threshold)
-cv.imshow('orig', img)
+# cv.imshow('threshold', threshold)
+# cv.imshow('orig', img)
 
-#2
 
 normalizedImg = np.zeros((150, 150))
 normalizedImg = cv.normalize(img,  normalizedImg, 0, 255, cv.NORM_MINMAX)
-cv.imshow('dst_rt', normalizedImg)
+# cv.imshow('dst_rt', normalizedImg)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
@@ -172,7 +142,7 @@ cv.destroyAllWindows()
 
 import imageio
 import numpy
-img = imageio.imread(r'D:\reposetory\Save_Transport\dataset\train\one_sec_cut.jpg\0_1_01.jpg')
+img = imageio.imread(r'/home/timur/Documents/Projects/sound_classif/git/Save_Transport/dataset/train/one_sec_cut.jpg/0_1_01.jpg')
 array = numpy.asarray(img)
 print(array.shape)
 
